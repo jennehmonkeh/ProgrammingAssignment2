dd8e7c3b5a5f00abb59193e82612b71594c995a3
Wilkinson <jennifer.wilkinson@AGR.GC.CA>
2021-04-12 10:35:20 -0400

    Two Functions for Assignment #2 Week3 of R Programming Course makeCacheMatrix & cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## 
+## 
+## function makeCacheMatrix: Initialize a matrix function with a matrix
+## the matrix must be able to be inverted or an error will occur
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  # set reinitizializes x & m, resets the cached matrix
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  ## return the original matrix
+  get <- function() x
+  ## use ftn "solve" to invert the matrix
+  setinv <- function(solve) m <<- solve
+  ## return the cached inverted matrix or null if empty
+  getinv <- function() m
+  ## create the S3 Object "list"
+  ## names the functions available and the associated variables in memory:
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## function cacheSolve: return the inverse of the matrix and cache it
+## if matrix already in the cache, return the cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
